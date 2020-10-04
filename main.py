f5d537413ae8834528b24938a3ac868b902143f6
akshatjain7703 <akshatjain7703@gmail.com>
2020-10-04 17:48:21 +0000

    Added circlearea.py

--- main.py
+++
@@ -1,6 +0,0 @@
-from math import pi
-
-print("Let's calculate the area of a circle. \n")
-r = float(input("Input the radius of the circle:"))
-area = pi * r * r
-print("The area of the circle with radius",r,"is: \n", area)
